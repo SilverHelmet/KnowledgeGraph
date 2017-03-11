ef6da2a5aa389b073b1adbbc9f891dc72e08f5f5
Haoran Li <lhrshitc@163.com>
2017-03-11 20:18:29 +0800

    init

---
+++ src/queryKG/query_util.py
@@ -0,0 +1,9 @@
+query_format = ' \
+PREFIX fb: <http://rdf.freebase.com/ns/> \
+SELECT {ret} \
+WHERE \
+{{ \
+ {query} \
+}} \
+'
+
---
+++ src/test/entity_test.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+es = set()
+for line in file('result/360_entity.txt'):
+    p = line.split("\t")
+    es.add(p[0])
+
+for line in file('result/360entity_data.txt'):
+    e = line.split('\t')[0]
+    if not e in es:
+        print e
