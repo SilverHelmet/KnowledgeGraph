0e8274ec4b945323ff38016ac993dd24ddcf9fdf
Haoran Li <lhrshitc@163.com>
2017-03-12 15:03:00 +0800

    init

--- scripts/load_main.sh
+++ scripts/load_main.sh
@@ -2,5 +2,11 @@ patterns="chunk00*ttl chunk01*ttl chunk02*ttl chunk03*ttl chunk04*ttl chunk05*tt
 for pattern in $patterns
 do
     isql 13093 dba dba exec="ld_dir('/home/lhr/KnowledgeGraph/data/freebase', '${pattern}', 'freebase');"
-    bash bulk_load.sh
+    isql 13093 dba dba exec="rdf_loader_run();" &
+    isql 13093 dba dba exec="rdf_loader_run();" &
+    isql 13093 dba dba exec="rdf_loader_run();" &
+    wait
+    isql 13093 dba dba exec="checkpoint;"
+
+
 done
