b7b26b18217f67a7e87e2b5c4295e1c4b2a2e396
Haoran Li <lhrshitc@163.com>
2017-03-12 15:02:23 +0800

    init

--- scripts/bulk_load.sh
+++ scripts/bulk_load.sh
@@ -1,8 +1,6 @@
 isql 13093 dba dba exec="rdf_loader_run();" &
 isql 13093 dba dba exec="rdf_loader_run();" &
 isql 13093 dba dba exec="rdf_loader_run();" &
-isql 13093 dba dba exec="rdf_loader_run();" &
-isql 13093 dba dba exec="rdf_loader_run();" &
 wait
 isql 13093 dba dba exec="checkpoint;"
 
