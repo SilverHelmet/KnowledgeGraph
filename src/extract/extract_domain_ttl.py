124d090d652af7a828dd8e0b342b760d5d3da87a
Haoran Li <lhrshitc@163.com>
2017-03-16 11:45:21 +0800

    v1

---
+++ src/extract/extract_domain_ttl.py
@@ -0,0 +1,32 @@
+import sys
+import glob
+from process_fb_result import get_domain
+
+def load_domain():
+    domains = set()
+    for line in file('result/freebase_domain/schema.domain.tsv'):
+        line = line.strip()
+        if line:
+            domains.add(domain)
+    return domains
+
+domains = load_domain()
+pref = 'result/freebase/'
+partterns = ["type.property.unique", 'type.property.unit', 'type.property.reverse_property', 'type.object.type', 
+'type.object.name', 'freebase.type_profile.instance_count', 'freebase.type_hints.mediator', 'freebase.type_hints.included_types', 
+'common.topic.description','type.property.schema', 'type.property.expected_type']
+partterns = [pref + p + "/*ttl" for p in partterns]
+
+outf = file('result/freebase_domain/schema.ttl', 'w')
+for p in partterns:
+    print "parttern is %s" %partterns
+    for filepath in glob.glob(p):
+        for line in file(filepath):
+            p = line.strip().split('\t')
+            domain = get_domain(p[0])
+            if domain is not None and domain in domains:
+                outf.write(line)
+
+outf.close()
+
+        
---
+++ src/extract/process_fb_result.py
@@ -0,0 +1,67 @@
+import os
+import sys
+
+
+fb_prefix = "http://rdf.freebase.com/ns/"
+
+def get_domain(uri):
+    global fb_prefix
+    uri = uri[1:-1]
+    if uri.startswith(fb_prefix):
+        uri = uri[len(fb_prefix):]
+    else:
+        return None
+    p = uri.split('.')
+    if len(p) <= 2:
+        return None
+    return '.'.join(p[:-2])
+
+def get_type(uri):
+    global fb_prefix
+    uri = uri[1:-1]
+    if uri.startswith(fb_prefix):
+        uri = uri[len(fb_prefix):]
+    else:
+        return None
+    p = uri.split('.')
+    if len(p) <= 2:
+        return None
+    return '.'.join(p[:-1])
+
+def add_to_dict(cnt, key):
+    if key is None:
+        return
+    if not key in cnt:
+        cnt[key] = 1
+    else:
+        cnt[key] += 1
+
+def print_cnt(cnt, out_filepath):
+    outf = file(out_filepath, 'w')
+    for key in sorted(cnt.keys(), reverse = True):
+        outf.write("%s\n" %(key))
+    outf.close()
+
+domain_cnt = {}
+type_cnt = {}
+
+for line in file(sys.argv[1]):
+    line = line.strip()
+    ttl = line.split('\t')
+    fb_domain = get_domain(ttl[0])
+    fb_type = get_type(ttl[0])
+    if fb_domain and not fb_domain.startswith("base") and not fb_domain.startswith('user') and not fb_domain.startswith('freebase'):
+        add_to_dict(domain_cnt, fb_domain)
+        add_to_dict(type_cnt, fb_type)
+
+    fb_domain = get_domain(ttl[2])
+    fb_type = get_type(ttl[2])
+    if fb_domain and not fb_domain.startswith("base") and not fb_domain.startswith('user') and not fb_domain.startswith('freebase'):
+        add_to_dict(domain_cnt, fb_domain)
+        add_to_dict(type_cnt, fb_type)
+
+
+print_cnt(domain_cnt, sys.argv[2])
+print_cnt(type_cnt, sys.argv[3])
+    
+    
