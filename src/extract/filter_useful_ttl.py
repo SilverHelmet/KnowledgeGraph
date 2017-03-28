14ef4c8816d90f11973be774579e10bde370a4e2
Haoran Li <lhrshitc@163.com>
2017-03-28 23:13:59 +0800

    v1

--- src/extract/filter_useful_ttl.py
+++ src/extract/filter_useful_ttl.py
@@ -1,5 +1,5 @@
 import sys
-from .extract_util import get_domain
+from .extract_util import get_domain, encode
 
 black_schema_domains = set(['fb:freebase', 'fb:base', 'fb:type', 'fb:m', 'fb:g', 'fb:user', 'fb:common'])
 def useful_domain(ttl):
@@ -9,6 +9,14 @@ def useful_domain(ttl):
     if domain is None:
         return False
     return not domain in black_schema_domains
+
+def filter_type(ttl):
+    uri = encode(ttl.split('\t')[2])
+    return uri == 'fb:type.type'
+
+def filter_property(ttl):
+    uri = encode(ttl.split("\t")[2])
+    return uri == 'fb:type.property'
 
     
 
@@ -27,4 +35,11 @@ def filter_ttl(in_filepath, out_filepath, valid_func):
 if __name__ == "__main__":
     in_filepath = sys.argv[1]
     out_filepath = sys.argv[2]
-    filter_ttl(in_filepath, out_filepath, useful_domain)
+    mode = sys.argv[3]
+    if mode == "schema":
+        func = useful_domain
+    elif mode == "type":
+        func = filter_type
+    elif mode == 'property':
+        func = filter_property
+    filter_ttl(in_filepath, out_filepath, func)
