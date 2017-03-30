e72b128f00967c4869fd3c1081bb263dad7f608a
Haoran Li <lhrshitc@163.com>
2017-03-30 09:15:11 +0800

    v1

--- src/extract/extract_subj.py
+++ src/extract/extract_subj.py
@@ -1,7 +1,26 @@
 from .extract_util import encode
 import sys
 
+def is_property(uri):
+    uri = encode(uri)
+    return uri == 'fb:type.property'
+
+def is_type(uri):
+    uri = encode(uri):
+    return uri == "fb:type.type"
+    
+    
 if __name__ == "__main__":
+    l = []
+    mode = sys.argv[1]
+    if mode == 'type':
+        func = is_type
+    elif mode == "property":
+        func = is_property
+
     for line in sys.stdin:
         p = line.strip().split("\t")
-        print encode(p[0])
+        if func(p[2]):
+            l.append(encode(p[0]))
+    for x in sorted(l):
+        print x
