bce67bb11d7734f5489804553bdc02e82765624f
Haoran Li <lhrshitc@163.com>
2017-04-11 00:08:56 +0800

    v1

--- src/schema/schema.py
+++ src/schema/schema.py
@@ -1,10 +1,13 @@
+from ..extract.extract_util import get_domain
 from ..IOUtil import doc_dir
 import os
 
 def load_types():
     types = set()
     for line in file(os.path.join(doc_dir, 'final_type_attrs.json'), 'r'):
-        types.add(line.strip().split("\t")[0])
+        type = line.strip().split("\t")[0]
+        if not get_domain(type) in ['fb:measurement_unit']:
+            types.add(type)
     return types
 
 if __name__ == "__main__":
