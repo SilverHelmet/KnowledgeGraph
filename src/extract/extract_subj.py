f5ecb44c0e56242cdbeb24642107e5e13a702a72
Haoran Li <lhrshitc@163.com>
2017-04-11 10:03:49 +0800

    v1

--- src/extract/extract_subj.py
+++ src/extract/extract_subj.py
@@ -8,6 +8,9 @@ def is_property(uri):
 def is_type(uri):
     uri = encode(uri)
     return uri == "fb:type.type"
+
+def is_true(uri):
+    return True
     
     
 if __name__ == "__main__":
@@ -18,6 +21,8 @@ if __name__ == "__main__":
         func = is_type
     elif mode == "property":
         func = is_property
+    elif mode == "True":
+        func = is_true
 
     for line in sys.stdin:
         p = line.strip().split("\t")
