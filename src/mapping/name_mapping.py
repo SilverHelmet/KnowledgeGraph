0e0a5d4a58d60cb35a92f05a082c4d43441ddee1
Haoran Li <lhrshitc@163.com>
2017-04-26 14:52:45 +0800

    v1

--- src/mapping/name_mapping.py
+++ src/mapping/name_mapping.py
@@ -21,6 +21,7 @@ def extract_str(attrs, name):
 def extract_name_mapping(attrs_path):
     Print("extract name mapping from %s" %attrs_path)
     name_mapping = {}
+    lower_name_mapping = {}
     for cnt, line in enumerate(file(attrs_path), start = 1):
         if cnt % 1000000 == 0:
             Print("\textract cnt = %d" %cnt)
@@ -30,14 +31,14 @@ def extract_name_mapping(attrs_path):
         key = p[0]
         attrs = json.loads(p[1])
         for name in extract_str(attrs, 'fb:type.object.name'):
-            tag = key + "#name"
-            add_to_dict_list(name_mapping, name, tag)
+            add_to_dict_list(name_mapping, name, key + "#name")
+            add_to_dict_list(lower_name_mapping, name.lower(), key + '#name_l')
 
         for name in extract_str(attrs, 'fb:common.topic.alias'):
-            tag = key + "#alias"
-            add_to_dict_list(name_mapping, name, tag)
+            add_to_dict_list(name_mapping, name, key + "#alias")
+            add_to_dict_list(lower_name_mapping, name.lower(), key + "#alias_l")
     
-    return name_mapping
+    return name_mapping, lower_name_mapping
 
 def mapping(name_mapping, name, res, key):
     if name is None:
@@ -47,7 +48,7 @@ def mapping(name_mapping, name, res, key):
         if not key in res:
             res[key] = []
         res[key].extend(name_mapping[name])
-        
+
         
 
 def do_mapping(name_mapping, baike_path, out_path):
@@ -88,13 +89,13 @@ def do_mapping(name_mapping, baike_path, out_path):
 if __name__ == "__main__":
     fb_attrs_path = os.path.join(result_dir, 'freebase/entity_attr.json')
     baike_attrs_path = os.path.join(result_dir, '360/360_entity_info.json')
-    out_path = os.path.join(result_dir, '360/360_mapping.json')
+    out_path = os.path.join(result_dir, '360/360_mapping_lowercase.json')
 
     # debug
     # fb_attrs_path = os.path.join(result_dir, 'test/fb_attr.json')
     # baike_attrs_path = os.path.join(result_dir, 'test/baike_attr.json')
     # out_path = os.path.join(result_dir, "test/mapping_result.json")
 
-    name_mapping = extract_name_mapping(fb_attrs_path)
-    do_mapping(name_mapping, baike_attrs_path, out_path)
+    name_mapping, lower_name_mapping = extract_name_mapping(fb_attrs_path)
+    do_mapping(lower_name_mapping, baike_attrs_path, out_path)
 
