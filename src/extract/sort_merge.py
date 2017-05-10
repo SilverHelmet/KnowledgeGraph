28974fcdc3f948e06a4a5b82f7f0c61fd4f9ac22
Haoran Li <lhrshitc@163.com>
2017-05-10 14:39:01 +0800

    v1

--- src/extract/sort_merge.py
+++ src/extract/sort_merge.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     out_file = os.path.join(result_dir, 'freebase/entity_property.json')
 
     # in_file = os.path.join(result_dir, 'test/entity_property.ttl')
-    out_file = os.path.join(result_dir, 'test/entity_property.json')
+    # out_file = os.path.join(result_dir, 'test/entity_property.json')
     property_map = {}
 
 
 
     for cnt, line in enumerate(file(in_file), start = 1):
-        if cnt % 10000 == 0:
+        if cnt % 1000000 == 0:
             Print("load cnt = %d" %cnt)
         p = line.split('\t')
         s = p[0]
@@ -25,6 +25,9 @@ if __name__ == "__main__":
         property_map[s].append((p[1], p[2]))
 
     outf = file(out_file, 'w')
-    for key in sorted(property_map.keys()):
+    Print("start sorting")
+    for cnt, key in enumerate(sorted(property_map.keys()), start = 0):
+        if cnt % 100000 == 0:
+            Print("write cnt = %d" %cnt)
         outf.write("%s\t%s\n" %(key, json.dumps(property_map[key])))
     outf.close()
