e347acb47bc23c0f7b7fefa622a00aa16996afbd
Haoran Li <lhrshitc@163.com>
2017-05-10 10:57:12 +0800

    v1

--- src/mapping/one2one_mapping_cnt.py
+++ src/mapping/one2one_mapping_cnt.py
@@ -6,36 +6,46 @@ import os
 def load_attrs():
     path = os.path.join(base_dir, 'docs/name_attr.txt')
     attrs = load_file(path)
-    attrs = [attr.strip().split("\t")[-1] for attr in attrs]
+    attrs = [attr.strip().split("\t")[-1].decode('utf-8') for attr in attrs]
     return attrs
 
 if __name__ == "__main__":
     attrs = load_attrs()
     out_path = os.path.join(result_dir, '360/mapping/exact_mapping.tsv')
     in_path = os.path.join(result_dir, '360/360_mapping.json')
-    
+
+    # debug
+    # in_path = os.path.join(result_dir, 'test/mapping_result.json')
+    # out_path = os.path.join(result_dir, 'test/exact_mapping.tsv')
+
     outf = file(out_path, 'w')
     
     one_cnt = 0
     cnt_map = {}
     for x in attrs:
         cnt_map[x] = 0
+
     for cnt, line in enumerate(file(in_path), start = 1):
         if cnt % 100000 == 0:
             Print("cnt = %d one_cnt = %d" %(cnt, one_cnt))
+        if cnt == 2:
+            break
+        
         key, obj = line.split('\t')
         obj = json.loads(obj)
         fb_uris = []
         for name_attr in attrs:
             if name_attr in obj:
-                fb_uris.extend(obj[name_attr])
+                fb_uris_list = [x.split("#")[0] for x in obj[name_attr]]
+                fb_uris.extend(fb_uris_list)
         fb_uris = set(fb_uris)
         if len(fb_uris) == 1:
             map_name = None
             for name in attrs:
                 if name in obj:
                     cnt_map[name] += 1
-                    map_name = name
+                    if map_name is None:
+                        map_name = name
             one_cnt += 1
 
             outf.write(key + "\t" + list(fb_uris)[0] + "\t" + map_name + '\n')
