0a9642e8b98b7cc8ed523e86c02bb87923a3e196
Haoran Li <lhrshitc@163.com>
2017-05-10 13:18:39 +0800

    v1

--- src/mapping/one2one_mapping_cnt.py
+++ src/mapping/one2one_mapping_cnt.py
@@ -51,10 +51,7 @@ if __name__ == "__main__":
     outf.close()
     print "one cnt = %d" %one_cnt
     for key in sorted(cnt_map.keys(), key = lambda x: cnt_map[x], reverse = True):
-        print type(key)
-        print key.encode('utf-8')
-        print sys.stdout.encoding
-        
+        key = key.encode('utf-8')
         print key, cnt_map[key]
 
     
