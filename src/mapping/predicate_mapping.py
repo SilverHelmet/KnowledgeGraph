1fa7ae0f6597993d764fc35472be1447a8ba4509
Haoran Li <lhrshitc@163.com>
2017-05-27 16:48:03 +0800

    v1

--- src/mapping/predicate_mapping.py
+++ src/mapping/predicate_mapping.py
@@ -49,7 +49,19 @@ def load_baike_info(filepath, total):
         info_map[key] = attr
     return info_map
 
+def load_exact_map(exact_map_path, total = 558541):
+    fb2baike = {}
+    Print("load exact map %s" %exact_map_path)
+    for line in tqdm(file(exact_map_path), total = total):
+        p = line.split('\t')
+        baike_url = p[0]
+        fb_uri = p[1]
+        fb2baike[fb_uri] = baike_url
+    return fb2baike
 
+def do_predicate_mapping(name_map, fb2baike, baike_entity_info, fb_property_path, total):
+    for line in tqdm(file(fb_property_path), total = total):
+        pass
 
 
 if __name__ == "__main__":
@@ -61,6 +73,12 @@ if __name__ == "__main__":
     # name_map = load_name_attr(name_files, totals)
 
     baike_entity_info_path = os.path.join(result_dir, '360/360_entity_info_processed.json')
-    baike_entity_info = load_baike_info(baike_entity_info_path, total = 21710208)
+    # baike_entity_info = load_baike_info(baike_entity_info_path, total = 21710208)
     
     print baike_entity_info['baike.so.com/doc/6692614-6906520.html']
+
+    exact_map_path = os.path.join(result_dir, '360/mapping/exact_mapping.tsv')
+    fb2baike = load_exact_map(exact_map_path)
+
+    # fb_property_path = os.path.join(result_dir, 'entity_property.json')
+    # do_predicate_mapping(name_map, fb2baike, baike_entity_info, fb_property_path, total = 53574900)
