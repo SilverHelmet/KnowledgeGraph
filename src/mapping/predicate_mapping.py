db77d3e8176add2c49de3f55b16184b85b3f87bd
Haoran Li <lhrshitc@163.com>
2017-05-27 16:32:13 +0800

    v1

--- src/mapping/predicate_mapping.py
+++ src/mapping/predicate_mapping.py
@@ -31,6 +31,26 @@ def load_name_attr(filepaths, totals):
                     name_map[key].append(name)
     return name_map
 
+def load_baike_info(filepath, total):
+    Print("load baike info from %s" %filepath)
+    info_map = {}
+    for line in tqdm(file(filepath), total = total):
+        p = line.split('\t')
+        key = p[0]
+        obj = json.loads(p[1])
+        attr = {}
+        if 'ename' in obj:
+            attr['ename'] = [obj['ename']]
+        if 'title' in obj:
+            attr['title'] = [obj['title']]
+        info = obj.get('info', {})
+        for name in info:
+            attr[name] = info[name]
+        info_map[key] = attr
+    return info_map
+
+
+
 
 if __name__ == "__main__":
     exact_mapping_file = os.path.join(result_dir, "360/mapping/exact_mapping.tsv")
@@ -38,5 +58,9 @@ if __name__ == "__main__":
                 os.path.join(result_dir, 'freebase/entity_alias.json')]
     totals = [39345270, 2197095]
 
-    name_map = load_name_attr(name_files, totals)
-    print name_map['fb:m.010016']
+    # name_map = load_name_attr(name_files, totals)
+
+    baike_entity_info_path = os.path.join(result_dir, '360/360_entity_info_processed.json')
+    baike_entity_info = load_dict(baike_entity_info_path, total = 21710208)
+    
+    print baike_entity_info['baike.so.com/doc/6692614-6906520.html']
--- src/test/test_time.py
+++ src/test/test_time.py
@@ -1,12 +1,12 @@
 #encoding: utf-8
 import dateparser
 
+time_strs = ['1962']
 
 for time_str in time_strs:
     print time_str
     print dateparser.parse(time_str, 
-        languages = ['zh'],
-        date_formats = [u'%Y年%m月'],
+        languages = ['zh', 'en'],
         settings = {"PREFER_DAY_OF_MONTH":"first"} )
 
 import datetime
