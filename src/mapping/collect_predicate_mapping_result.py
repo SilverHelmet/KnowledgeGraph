88228136896a05079bb0f6349648d8001e1288f1
Haoran Li <lhrshitc@163.com>
2017-05-30 13:56:32 +0800

    v1

---
+++ src/mapping/collect_predicate_mapping_result.py
@@ -0,0 +1,4 @@
+from ..IOUtil import result_dir
+
+if __name__ == "__main__":
+    
--- src/test/test_extend_fb_ttls.py
+++ src/test/test_extend_fb_ttls.py
@@ -7,15 +7,20 @@ if __name__ == "__main__":
     mediator_ttl_map = load_ttl2map(os.path.join(result_dir, 'freebase/mediator_property.ttl'), total = 50413655)
     fb_property_path = os.path.join(result_dir, 'freebase/entity_property.json')
 
+    pcnt = 0
     for cnt, line in enumerate(file(fb_property_path), start = 1):
         p = line.split('\t')
         fb_uri = p[0]
         fb_ttls = json.loads(p[1])
         new_fb_ttls = extend_fb_ttls(fb_ttls, fb_uri, mediator_ttl_map)
+        
         if len(new_fb_ttls)  > len(fb_ttls):
             print "------------------"
+            print fb_uri
             print fb_ttls
             print new_fb_ttls
-            break
+            pcnt += 1
+            if pcnt > 10:
+                break
 
 
