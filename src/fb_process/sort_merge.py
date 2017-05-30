278b93455bcd7a805b90dc5c0992af9ada15b6a3
Haoran Li <lhrshitc@163.com>
2017-05-30 12:59:05 +0800

    v1

--- src/fb_process/sort_merge.py
+++ src/fb_process/sort_merge.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     property_map = {}
 
-    mediator_ttl_map = load_ttl2map(os.path.join(result_dir, 'freebase/mediator_property.ttl'), total = 50413655)
+    # mediator_ttl_map = load_ttl2map(os.path.join(result_dir, 'freebase/mediator_property.ttl'), total = 50413655)
 
     for line in tqdm(file(in_file), total  = 283388281):
         p = line.strip().split('\t')
@@ -34,13 +34,13 @@ if __name__ == "__main__":
     Print("start sorting")
     for key in tqdm(sorted(property_map.keys()), total = len(property_map)):
         ttls = property_map[key]
-        mediator_ttls = []
-        p_map = {'property': ttls}
-        for _, value in ttls:
-            if value in mediator_ttl_map:
-                mediator_ttls.extend(mediator_ttl_map[value])
-        if len(mediator_ttls) > 0:
-            p_map['mediator_property'] = mediator_ttls
-        outf.write("%s\t%s\n" %(key, json.dumps(p_map)))
+        # mediator_ttls = []
+        # p_map = {'property': ttls}
+        # for _, value in ttls:
+        #     if value in mediator_ttl_map:
+        #         mediator_ttls.extend(mediator_ttl_map[value])
+        # if len(mediator_ttls) > 0:
+        #     p_map['mediator_property'] = mediator_ttls
+        outf.write("%s\t%s\n" %(key, json.dumps(ttls)))
     outf.close()
     Print("Write Over")
