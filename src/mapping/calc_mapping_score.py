cfd63aaa8cf6215bd072c5f2844a0185c771bb39
Haoran Li <lhrshitc@163.com>
2017-08-30 21:57:52 +0800

    v1

--- src/mapping/calc_mapping_score.py
+++ src/mapping/calc_mapping_score.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
 	print len(name_map)
 	mediator_ttl_map = load_ttl2map(os.path.join(result_dir, 'freebase/mediator_med_property.ttl'), total = 50413655, entities = set(fb_entities) )
 
-	# print len(mediator_ttl_map)
+	print len(mediator_ttl_map)
 
 	# baike_entity_info_path = os.path.join(result_dir, '360/360_entity_info_processed.json')
 	# baike_entity_info = load_baike_info(baike_entity_info_path, total = 21710208, baike_entities)
