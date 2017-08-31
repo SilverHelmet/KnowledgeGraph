d54d7748858141e383aa693485642c8ed42fa9cf
Haoran Li <lhrshitc@163.com>
2017-08-31 20:51:42 +0800

    v1

--- src/mapping/classify/check.py
+++ src/mapping/classify/check.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     fb_entity_info = load_json_map(fb_entity_info_path, total = 6282988)
 
     mapping_file = os.path.join(result_dir, '360/360_mapping.json')
-	baike2fb_map, baike_entities, fb_entities = load_mapping_pairs(mapping_file)
 
+    baike2fb_map, baike_entities, fb_entities = load_mapping_pairs(mapping_file)
+    fb_entities = set(fb_entities)
     for key in fb_entities:
         if not key in fb_entity_info:
             print key
