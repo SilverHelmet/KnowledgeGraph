2cd97ed0db39184871b88777210bc1fd9899d9ab
Haoran Li <lhrshitc@163.com>
2017-08-31 21:50:59 +0800

    v1

--- src/mapping/classify/calc_infobox_mapping_score.py
+++ src/mapping/classify/calc_infobox_mapping_score.py
@@ -67,28 +67,28 @@ def calc_infobox_mapping_score(baike2fb_map, baike_entitiy_info, fb_entity_info,
 
 
 if __name__ == "__main__":
-	mapping_file = os.path.join(result_dir, '360/360_mapping.json')
-	out_dir = os.path.join(result_dir, '360/mapping/classify')
-	baike2fb_map, baike_entities, fb_entities = load_mapping_pairs(mapping_file)
-	print "baike2fb_map", len(baike2fb_map)
-	print "baike_entities", len(baike_entities)
-	print "fb_entities", len(fb_entities)
+    mapping_file = os.path.join(result_dir, '360/360_mapping.json')
+    out_dir = os.path.join(result_dir, '360/mapping/classify')
+    baike2fb_map, baike_entities, fb_entities = load_mapping_pairs(mapping_file)
+    print "baike2fb_map", len(baike2fb_map)
+    print "baike_entities", len(baike_entities)
+    print "fb_entities", len(fb_entities)
 
     baike_name_attrs = load_attrs()
 
     baike_entity_info_path = os.path.join(result_dir, '360/360_entity_info_processed.json')
-	baike_entity_info = load_baike_info(baike_entity_info_path, total = 21710208, entities = baike_entities)
+    baike_entity_info = load_baike_info(baike_entity_info_path, total = 21710208, entities = baike_entities)
     baike_name_attrs = load_attrs()
-	print "baike_entitiy_info", len(baike_entity_info)
+    print "baike_entitiy_info", len(baike_entity_info)
 
     fb_entity_info_path = os.path.join(result_dir, '360/mapping/classify/mapped_fb_entity_info.json')
     fb_entity_info = load_json_map(fb_entity_info_path, total = 6282988)
 
 
-	name_files = [os.path.join(result_dir, 'freebase/entity_name.json'),
-			os.path.join(result_dir, 'freebase/entity_alias.json')]
-	totals = [39345270, 2197095]
-	name_map = load_name_attr(name_files, totals, set(fb_entities))
+    name_files = [os.path.join(result_dir, 'freebase/entity_name.json'),
+            os.path.join(result_dir, 'freebase/entity_alias.json')]
+    totals = [39345270, 2197095]
+    name_map = load_name_attr(name_files, totals, set(fb_entities))
 
     map_scores = calc_infobox_mapping_score(baike2fb_map, baike_entity_info, fb_entity_info, name_map, baike_name_attrs)
     out_path = os.path.join(out_dir, 'map_scores.json')
@@ -97,12 +97,12 @@ if __name__ == "__main__":
         outf.write(json.dumps(map_obj) + '\n')
     outf.close()
     
-	# print "name_map", len(name_map)
-	# description_map = load_name_attr([os.path.join(result_dir, 'freebase/entity_description.json')], totals = [6426977], fb_entities = set(fb_entities))
-	
+    # print "name_map", len(name_map)
+    # description_map = load_name_attr([os.path.join(result_dir, 'freebase/entity_description.json')], totals = [6426977], fb_entities = set(fb_entities))
+    
 
 
 
 
 
-	
+    
