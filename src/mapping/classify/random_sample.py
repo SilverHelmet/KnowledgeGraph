cf3903bdd480fa3852c5ab71a843605a86c43602
Haoran Li <lhrshitc@163.com>
2017-09-10 17:07:11 +0800

    v1

--- src/mapping/classify/random_sample.py
+++ src/mapping/classify/random_sample.py
@@ -2,6 +2,7 @@ from ...IOUtil import result_dir, Print
 import json
 from tqdm import tqdm
 import os
+import numpy as np
 
 def count_occur(mapping_path):
     Print("count occur from [%s]" %mapping_path)
@@ -18,7 +19,31 @@ def count_occur(mapping_path):
             fb_cnt[fb_uri] +=1
     return baike_cnt, fb_cnt
 
+def generate_sample(baike_urls, fb_uris, out_path, mapping_path):
+    Print('generate sample')
+    fb_lines = {}
+    for fb in fb_uris:
+        fb_lines[fb] = []
 
+    outf = file(out_path, 'w')
+
+    for line in tqdm(file(mapping_path), total = 1129601):
+        p = line.split('\t')
+        baike_url = p[0]
+        if baike_url in baike_urls:
+            outf.write(line)
+        else:
+            obj = [w for w in json.loads(p[1])]
+            add_cnt = 0
+            for fb in obj:
+                if fb in fb_lines:
+                    add_cnt += 1
+                    fb_lines[fb].append(line)
+            assert add_cnt < 2
+
+    for fb in sorted(fb_lines.keys()):
+        outf.write("".join(fb_lines[fb]))
+    outf.close()
 
 if __name__ == "__main__":
     baike_urls = set(['baike.so.com/doc/10066508-10591843.html', 
@@ -29,16 +54,35 @@ if __name__ == "__main__":
     fb_uris = set(['fb:m.02hrt7', 'fb:m.022fj_'])
 
 
-    small_rest = 40
-    big_rest = 40
-    fb2baike_big_rest = 20
+    small_rest = 40 #  2 - 5
+    big_rest = 40 # 6 - 20
+    fb2baike_big_rest = 20 # >= 3
 
     base_dir = os.path.join(result_dir, '360/mapping/classify')
     mapping_path = os.path.join(base_dir, 'mappings.txt')
     baike_cnt, fb_cnt = count_occur(mapping_path)
 
-    for bk in baike_urls:
-        print bk, baike_cnt[bk]
+    baike_small = random_sample(baike_cnt, small_rest, lambda x: x >=2 and x <= 5)
+    baike_big = random_sample(baike_cnt, big_rest, lambda x: x >= 6 and x <= 20)
+    fb_big = random_sample(fb_cnt, fb2baike_big_rest, lambda x: x >= 3)
 
-    for fb in fb_uris:
-        print fb, fb_cnt[fb]
+    baike_urls.update(baike_small)
+    baike_urls.update(baike_big)
+    fb_uris.update(fb_big)
+
+    cnt = 0
+    cnt += len(baike_urls)
+    for fb in fb_cnt:
+        cnt += fb_cnt[fb]
+    
+    print "cnt is %d" %cnt
+
+    
+    out_path = os.path.join(base_dir, 'train_data/train_data.json')
+    generate_sample(baike_urls, fb_uris, out_path, mapping_path)
+    
+
+
+
+
+    
