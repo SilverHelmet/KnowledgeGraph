0416da4097e272c0c2e01c212485651c39ae633a
Haoran Li <lhrshitc@163.com>
2017-09-10 16:44:37 +0800

    v1

--- src/mapping/classify/random_sample.py
+++ src/mapping/classify/random_sample.py
@@ -7,9 +7,7 @@ def count_occur(mapping_path):
     Print("count occur from [%s]" %mapping_path)
     baike_cnt = {}
     fb_cnt = {}
-    for idx, line in enumerate(tqdm(file(mapping_path), total = 1129601)):
-        if idx >= 100:
-            break
+    for line in tqdm(file(mapping_path), total = 1129601):
         p = line.split('\t')
         baike_url = p[0]
         obj = [w.encode('utf-8') for w in json.loads(p[1])]
@@ -26,8 +24,9 @@ if __name__ == "__main__":
     baike_urls = set(['baike.so.com/doc/10066508-10591843.html', 
     'baike.so.com/doc/1287918-1361771.html',
     'baike.so.com/doc/5807317-6020118.html',
-    'baike.so.com/doc/6773200-6988367.html'])
-    fb_uris = set(['fb:m.02hrt7', 'fb:m.022fj_', 'fb:m.022fj', 'fb:m.023m6pg'])
+    'baike.so.com/doc/6773200-6988367.html',
+    'baike.so.com/doc/5363940-5599529.html'])
+    fb_uris = set(['fb:m.02hrt7', 'fb:m.022fj_'])
 
 
     small_rest = 40
@@ -38,16 +37,8 @@ if __name__ == "__main__":
     mapping_path = os.path.join(base_dir, 'mappings.txt')
     baike_cnt, fb_cnt = count_occur(mapping_path)
 
-    for key in fb_cnt.keys()[:10]:
-        print "test", key, type(key)
-
-    # for bk in baike_urls:
-    #     print bk, baike_cnt[bk]
-
-
-    # print ""
-    # print ""
-
+    for bk in baike_urls:
+        print bk, baike_cnt[bk]
 
     for fb in fb_uris:
         print fb, fb_cnt[fb]
