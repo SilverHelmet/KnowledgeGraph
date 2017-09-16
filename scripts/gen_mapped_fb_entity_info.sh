467992b90d8ef837b8f651fa066f902c53d8c20b
Haoran Li <lhrshitc@163.com>
2017-09-17 02:49:14 +0800

    v1

--- scripts/gen_mapped_fb_entity_info.sh
+++ scripts/gen_mapped_fb_entity_info.sh
@@ -1,4 +1,4 @@
-python -u -m src.fb_process.extract_mediator >& log/extract_mediator.log 
+# python -u -m src.fb_process.extract_mediator >& log/extract_mediator.log 
 python -u -m src.fb_process.filter_useful_ttl result/freebase/mediator_property.ttl  result/freebase/mediator_med_property.ttl property_mediator_ttl 50413655 >& log/filter_mediator_med_property.log 
 python -u -m src.mapping.classify.gen_fb_property >& log/gen_fb_property.log 
 python -u -m src.mapping.classify.gen_fb_property_name >& log/gen_fb_property_name.log 
