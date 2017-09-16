ff154dc306a775bd9564aa0c9aa23932b14c04d4
Haoran Li <lhrshitc@163.com>
2017-09-17 00:33:47 +0800

    v1

--- scripts/gen_mapped_fb_entity_info.sh
+++ scripts/gen_mapped_fb_entity_info.sh
@@ -1,2 +1,3 @@
 python -u -m src.mapping.classify.gen_fb_property >& log/gen_fb_property.log 
 python -u -m src.mapping.classify.gen_fb_property_name >& log/gen_fb_property_name.log 
+python -u -m src.mapping.classify.calc_infobox_mapping_score >& log/calc_mapping_score.log
