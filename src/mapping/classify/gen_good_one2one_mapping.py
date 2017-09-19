bd21babfbe30a25929b79056a57e2921767071bf
Haoran Li <lhrshitc@163.com>
2017-09-19 21:23:55 +0800

    v1

--- src/mapping/classify/gen_good_one2one_mapping.py
+++ src/mapping/classify/gen_good_one2one_mapping.py
@@ -1,8 +1,10 @@
 from ...IOUtil import result_dir, Print
 import os
 
+
 if __name__ == "__main__":
     base_dir = os.path.join(result_dir, '360/mapping/classify')
     map_filepath = os.path.join(base_dir, 'mappings.txt')
+
 
     
---
+++ src/mapping/classify/util.py
@@ -0,0 +1,21 @@
+import os
+from ...IOUtil import result_dir, Print
+from tqdm import tqdm
+import json
+
+def load_mappings():
+    mapping_path = os.path.join(result_dir, '360/mapping/classify/mappings.txt')
+    Print('load mappings from [%s]' %mapping_path)
+    total = 1129601
+    baike2fb = {}
+    for line in tqdm(file(mapping_path), total = total):
+        p = line.split('\t')
+        baike_url = p[0]
+        fb_uris = json.loads(p[1])
+        baike2fb[baike_url] = fb_uris
+    return baike2fb
+
+
+
+if __name__ == "__main__":
+    print len(load_mappings())
