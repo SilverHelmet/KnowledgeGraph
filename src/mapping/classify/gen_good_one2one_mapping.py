45d82be67472f13191070f1c17290b991d8d893f
Haoran Li <lhrshitc@163.com>
2017-09-20 00:35:31 +0800

    v1

--- src/mapping/classify/gen_good_one2one_mapping.py
+++ src/mapping/classify/gen_good_one2one_mapping.py
@@ -1,10 +1,60 @@
 from ...IOUtil import result_dir, Print
+from .util import load_mappings
+from .simple_classify import SimpleClassifer, make_key
 import os
+
+def count_entity(baike2fb):
+    bk_cnt = {}
+    fb_cnt = {}
+    for bk_url in baike2fb:
+        for fb_uri in baike2fb[bk_url]:
+            if not bk_url in bk_cnt:
+                bk_cnt[bk_url] = 0
+            if not fb_uri in fb_cnt:
+                fb_cnt[fb_uri] = 0
+            bk_cnt[bk_url] += 1
+            fb_cnt[fb_uri] += 1
+    return bk_cnt, fb_cnt
 
 
 if __name__ == "__main__":
-    base_dir = os.path.join(result_dir, '360/mapping/classify')
-    map_filepath = os.path.join(base_dir, 'mappings.txt')
+    baike2fb = load_mappings()
+    bk_cnt, fb_cnt = count_entity(baike2fb)
+    
+    one2one_mapping_pairs = []
+    for bk_url in bk_cnt:
+        if bk_cnt[bk_url] != 1:
+            continue
+        fb_uri = baike2fb[bk_url][0]
+        if fb_cnt[fb_uri] != 1:
+            continue
+        one2one_mapping_pairs.append(bk_url, fb_uri)
+    Print("#one2one mapping pairs = %d" %len(one2one_mapping_pairs))
+
+    clf = SimpleClassifer(1, 1)
+    clf.load_score(good_mapping_pairs)
+    score_map = clf.calc_score(good_mapping_pairs)
+
+    good_mapping_pairs = []
+    for bk_url, fb_uri in one2one_mapping_pairs:
+        key = make_key(bk_url, fb_uri)
+        score = score_map[key]
+        if score > 0.1:
+            good_mapping_pairs.append((bk_url, fb_uri, score))
+
+    Print("#good mapping pairs = %d" %len(good_mapping_pairs))
+
+    out_path = os.path.join(result_dir, '360/mapping/classify/good_one2one_mappings.txt')
+    outf = file(out, 'w')
+    for bk_url, fb_uri, score in good_mapping_pairs:
+        outf.write("%s\t%s\t%f\n", %(bk_url, fb_uri, score))
+    outf.close()
+
+
+
+
+
+
 
 
     
