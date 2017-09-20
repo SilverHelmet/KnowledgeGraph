9e0abd370072bdb818186dd82ca2fb3c944c382a
Haoran Li <lhrshitc@163.com>
2017-09-20 21:05:36 +0800

    v1

---
+++ src/mapping/classify/extract_baike_class.py
@@ -0,0 +1,21 @@
+from .util import load_baike_entity_class, load_mappings
+from ...IOUtil import result_dir, Print
+import os
+
+if __name__ == "__main__":
+    baike2fb = load_mappings()
+    baike_urls =set(baike2fb.keys())
+    baike2cls = load_baike_entity_class(baike_urls = baike_urls)
+    Print('load class %d/%d' %(len(baike2cls, len(baike_urls))))
+    values = set(baike2cls.values())
+    for value in values:
+        print value
+    Print("#value = %d" %len(values))
+
+    out_path = os.path.join(result_dir, '360/mapping/classify/baike_cls.tsv')
+    outf = file(out_path, 'w')    
+    for baike_url in sorted(baike2cls.keys()):
+        outf.write('%s\t%s\n' %(baike_url, baike2cls[baike_url]))
+    outf.close()
+
+    
--- src/mapping/classify/util.py
+++ src/mapping/classify/util.py
@@ -1,9 +1,9 @@
 import os
-from ...IOUtil import result_dir, Print
+from ...IOUtil import result_dir, Print, data_dir, nb_lines_of
 from tqdm import tqdm
 import json
 
-def load_mappings(with_score = False):
+def load_mappings():
     mapping_path = os.path.join(result_dir, '360/mapping/classify/mappings.txt')
     Print('load mappings from [%s]' %mapping_path)
     total = 1129601
@@ -12,13 +12,23 @@ def load_mappings(with_score = False):
         p = line.split('\t')
         baike_url = p[0]
         fb_uris = json.loads(p[1])
-        if with
         baike2fb[baike_url] = fb_uris
     return baike2fb
 
-def load_
-
+def load_baike_entity_class(filepath = None, baike_urls = None):
+    if filepath is None:
+        filepath = os.path.join(data_dir, 'entityHypernym_v16_all_ex_darts.txt')
+    bk2cls = {}
+    for line in tqdm(file(filepath), total = nb_lines_of(filepath)):
+        name, popular, url, cls, small_cl = line.strip().split('\t')
+        if cls == "NO_TYPE":
+            continue
+        assert url.startswith('http://')
+        url = url[7:]
+        if baike_urls is None or url in baike_urls:
+            bk2cls[url] = cls
+    return bk2cls
 
 
 if __name__ == "__main__":
-    print len(load_mappings())
+    pass
