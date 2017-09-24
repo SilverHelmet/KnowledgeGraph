c5396e84277330699722257057f45f3936ab6e9d
Haoran Li <lhrshitc@163.com>
2017-09-24 15:18:26 +0800

    v1

--- src/test/test_jieba.py
+++ src/test/test_jieba.py
@@ -9,12 +9,9 @@ jieba.initialize()
 # t m x 时间
 print jieba.get_dict_file()
 s = '看过刘德华的《陆上行舟》，或许会感叹其作品的超凡脱俗，也或许正是这样，不了解沃纳的作品的人是很难接受其风格的。他的另一部作品《史楚锡流浪记》，也是不可多得的精品。'
-s = u"33厘米"
+s = u'刘德华出生于1993年11月29日, 1883.3'
 print BaikeDatetime.parse(s)
 words = pseg.cut(s)
-words = [ x for x in words]
 for word, flag in words:
     print('%s %s' % (word, flag))
 
-for word, flag in words:
-    print('%s %s' % (word, flag))
