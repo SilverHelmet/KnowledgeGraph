b09cd04c932616bd86eb3416eff272f01d686659
Haoran Li <lhrshitc@163.com>
2017-09-24 19:49:08 +0800

    v1

--- src/rel_extraction/process_jieba_dict.py
+++ src/rel_extraction/process_jieba_dict.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     dict_path = os.path.join(rel_ext_dir, 'baike_dict.txt')
     processed_path = os.path.join(rel_ext_dir, 'trimmed_baike_dict.txt')
     re_eng = re.compile(r"^[a-zA-Z]+$")
-    re_digit = re.compile(r'^[0-9=-+!?]+$')
+    re_digit = re.compile(r'^[0-9+-=!?]+$')
 
     outf = file(processed_path, 'w')
     for line in tqdm(file(dict_path), total = nb_lines_of(dict_path)):
