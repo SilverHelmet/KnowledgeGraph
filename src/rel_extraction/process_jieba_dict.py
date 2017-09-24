b46be9753518fc5f4f841310482bc127359c0589
Haoran Li <lhrshitc@163.com>
2017-09-24 19:47:44 +0800

    v1

--- src/rel_extraction/process_jieba_dict.py
+++ src/rel_extraction/process_jieba_dict.py
@@ -8,11 +8,14 @@ from tqdm import tqdm
 if __name__ == "__main__":
     dict_path = os.path.join(rel_ext_dir, 'baike_dict.txt')
     processed_path = os.path.join(rel_ext_dir, 'trimmed_baike_dict.txt')
-    re_eng = re.compile("^[a-zA-Z0-9]+$")
+    re_eng = re.compile("^[a-zA-Z]+$")
+    re_digit = re.compile('^[0-9=-+!?]+$')
 
     outf = file(processed_path, 'w')
     for line in tqdm(file(dict_path), total = nb_lines_of(dict_path)):
         word, freq, tag = line.strip().decode('utf-8').split(' ')
+        if re_digit.match(word):
+            continue
         words = [x for x in jieba.cut(word)]
         if len(words) != 1:
             outf.write(line)
