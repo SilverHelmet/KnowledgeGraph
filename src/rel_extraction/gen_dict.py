199dd4fdb05ce5dc9cc90d63b1dbaefd6d7869fc
Haoran Li <lhrshitc@163.com>
2017-09-24 13:42:52 +0800

    v1

--- src/rel_extraction/gen_dict.py
+++ src/rel_extraction/gen_dict.py
@@ -1,4 +1,22 @@
 import os
+from .util import load_bk_entity_pop, load_name2baike
+from ..IOUtil import rel_ext_dir, Print
+from tqdm import tqdm
 
 if __name__ == "__main__":
+    pop_map = load_bk_entity_pop()
+    name2bk = load_name2baike()
+    keys = sorted(name2bk)
+    out_path = os.path.join(rel_ext_dir, 'baike_dict.txt')
+    outf = file(out_path, 'w')
+    Print('write dict to %s' %out_path)
+    for name in tqdm(keys, total = len(keys)):
+        bks = name2bk[name]
+        pop = 0
+        for bk_url in bks:
+            pop = max(pop, pop_map.get(bk_url, 0))
+        ouf.write('%s %d baike' %(name, pop))
+    outf.close()
+
+
     
--- src/rel_extraction/util.py
+++ src/rel_extraction/util.py
@@ -13,7 +13,7 @@ def load_mappings(filepath = None):
         bk2fb[bk_url] = fb_uri
     return bk2fb
 
-def load_entity_pop(filepath = None):
+def load_bk_entity_pop(filepath = None):
     if filepath is None:
         filepath = os.path.join(rel_ext_dir, 'baike_static_info.tsv')
         total = 21710208
@@ -21,9 +21,32 @@ def load_entity_pop(filepath = None):
         total = nb_lines_of(filepath)
     Print('load entity popularity from [%s]'  %filepath)
     
+    pop_map = {}
     for line in tqdm(file(filepath), total = total):
         p = line.strip().decode('utf-8').split('\t')
         bk_url = p[0]
-        
+        pop = int(p[2])
+        pop_map[bk_url] = pop
+    return pop_map
+
+def load_name2baike(filepath = None):
+    if filepath is None:
+        filepath = os.path.jion(rel_ext_dir, 'baike_names.tsv')
+        total = None
+    else:
+        total = nb_lines_of(filepath)
+    name2bk = {}
+    for line in file(filepath):
+        p = line.strip().decode('utf-8').split('\t')
+        bk_url = p[0]
+        names = p[1:]
+        for name in names:
+            if not name in name2bk:
+                name2bk[name] = []
+            name2bk[name].append(bk_url)
+    return name2bk
+
+
+
 
 
