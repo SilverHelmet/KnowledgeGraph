898b7ca8f60c2e8ffbae211d51ee1f3ce7d0068a
LiuShixing <1576168346@qq.com>
2017-10-05 16:37:58 +0800

    v1

--- src/extractor/entity/ner.py
+++ src/extractor/entity/ner.py
@@ -4,11 +4,11 @@ from pyltp import Postagger
 from pyltp import NamedEntityRecognizer
 from pyltp import Parser
 
-LTP_PATH="LTP/ltp_data_v3.4.0"
+LTP_PATH="../../../../LTP/ltp_data_v3.4.0"
 SEG_PATH=LTP_PATH+"/cws.model"
 POS_PATH=LTP_PATH+"/pos.model"
 REG_PATH=LTP_PATH+"/ner.model"
-PARSER_PATH=LTP_PATH+"/parser.model"
+PARSER_PATH=LTP_PATH+"/parser.model" 
 
 class NamedEntityReg:
 
@@ -27,8 +27,10 @@ class NamedEntityReg:
 
 	def recognize(self,sentence,nlp_res,page_info):
 		words=self.segment(sentence)
-		raw_entitys=list(self.__recognizer.recognize(words,self.postag(words)))
-		return raw_entitys,self.optimize_entitys(raw_entitys)
+		postag=self.postag(words)
+		raw_entitys=list(self.__recognizer.recognize(words,postag))
+		optimize_entitys=self.__optimize_entitys(postag,raw_entitys)
+		return raw_entitys,self.__entity_tuples(optimize_entitys)
 
 	def segment(self,sentence):
 		words=self.__segmenter.segment(sentence)
@@ -48,8 +50,68 @@ class NamedEntityReg:
 		self.__recognizer.release()
 		self.__parser.release()
 
-	def optimize_entitys(self,raw_entitys):
-		return list( (st,st+1)  for st in range(len(raw_entitys) ) if raw_entitys[st] != "O" )
+	def __entity_tuples(self,entitys):
+		tuples=[]
+		index=0
+		while index < len(entitys):
+			if entitys[index] != "O":
+				if entitys[index].split("-")[0] == "S":
+					tuples.append((index,index+1))
+				elif entitys[index].split("-")[0] == "B":
+					begin=index
+					while entitys[index].split("-")[0] != "E":
+						index+=1
+					tuples.append((begin,index+1))
+			index+=1
+		return tuples
+
+	def __optimize_entitys(self,postag,raw_entitys):
+		new_entitys=self.__pos_nh_to_ner_nh(postag,raw_entitys)
+		return new_entitys
+
+
+	"""
+	将postag词性为nh的词判定为实体Nh
+	"""
+	def __pos_nh_to_ner_nh(self,postag,raw_entitys):
+		new_entitys=raw_entitys
+		pos_nh_lists=self.__find_pos_nh_lists(postag)
+		for t in pos_nh_lists:
+			for i in range(len(t)-1):
+				new_entitys[t[i]]='I-Nh'
+			if len(t)==2:
+				new_entitys[t[0]]='S-Nh'
+			else:
+				 new_entitys[t[0]]='B-Nh'
+				 new_entitys[t[len(t)-2]]='E-Nh'
+		return new_entitys
+		
+
+	def __find_pos_nh_lists(self,postag):
+		L=[]
+		index=0
+		while index <len(postag):
+			t=[]
+			if postag[index]=='nh':
+				while index < len(postag) and postag[index]=='nh':
+					t.append(index)
+					index+=1
+				t.append(index)				
+				L.append(t)
+			index+=1
+		return L
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
