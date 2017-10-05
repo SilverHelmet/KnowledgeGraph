c20c696f7529519b36c6b6ddbfae5ba0fa4728e3
LiuShixing <1576168346@qq.com>
2017-10-05 20:31:00 +0800

    v1

--- src/extractor/entity/ner.py
+++ src/extractor/entity/ner.py
@@ -1,47 +1,37 @@
+# -*- coding: utf-8 -*-
 
 from pyltp import Segmentor
 from pyltp import Postagger
 from pyltp import NamedEntityRecognizer
 from pyltp import Parser
-
-LTP_PATH="../../../../LTP/ltp_data_v3.4.0"
-SEG_PATH=LTP_PATH+"/cws.model"
-POS_PATH=LTP_PATH+"/pos.model"
-REG_PATH=LTP_PATH+"/ner.model"
-PARSER_PATH=LTP_PATH+"/parser.model" 
+import sys
+sys.path.append("..")
+import ltp
 
 class NamedEntityReg:
 
-	def __init__(self,segmenter_path,postagger_path,recognizer_path,parser_path):
-		self.__segmenter=Segmentor()
-		self.__segmenter.load(segmenter_path)
+	def __init__(self):
 
-		self.__postagger=Postagger()
-		self.__postagger.load(postagger_path)
+		self.__ltp = LTP("lib/ltp_data_v3.4.0")
 
-		self.__recognizer=NamedEntityRecognizer()
-		self.__recognizer.load(recognizer_path)
+	def recognize(self,sentence,LTPResult,page_info):
+		words = self.segment(sentence)
+		postag = self.postag(words)
+		new_words,new_postag = self.__optimize_segment(words,postag)
 
-		self.__parser=Parser()
-		self.__parser.load(parser_path)
+		raw_entitys = list(self.__recognizer.recognize(new_words,new_postag))
 
-	def recognize(self,sentence,nlp_res,page_info):
-		words=self.segment(sentence)
-		postag=self.postag(words)
-		raw_entitys=list(self.__recognizer.recognize(words,postag))
-		optimize_entitys=self.__optimize_entitys(postag,raw_entitys)
+		new_raw_entitys = list(self.__ltp.nertagger.recognize(words,postag))
+		print("原实体：",raw_entitys,"\n","新实体",new_raw_entitys,"\n")
+		optimize_entitys = self.__optimize_entitys(postag,raw_entitys)
 		return raw_entitys,self.__entity_tuples(optimize_entitys)
 
 	def segment(self,sentence):
-		words=self.__segmenter.segment(sentence)
+		words = self.__ltp.segmentor.segment(sentence)
 		return list(words)
 	
 	def postag(self,words):
-		return list(self.__postagger.postag(words))
-
-	def parse(self,words,postag):
-		pres=self.__parser.parse(words,postag)
-		return list((item.head,item.relation) for item in pres)
+		return list(self.__ltp.tagger.postag(words))
 
 
 	def release(self):
@@ -51,22 +41,22 @@ class NamedEntityReg:
 		self.__parser.release()
 
 	def __entity_tuples(self,entitys):
-		tuples=[]
-		index=0
+		tuples = []
+		index = 0
 		while index < len(entitys):
 			if entitys[index] != "O":
 				if entitys[index].split("-")[0] == "S":
 					tuples.append((index,index+1))
 				elif entitys[index].split("-")[0] == "B":
-					begin=index
+					begin = index
 					while entitys[index].split("-")[0] != "E":
-						index+=1
+						index += 1
 					tuples.append((begin,index+1))
-			index+=1
+			index += 1
 		return tuples
 
 	def __optimize_entitys(self,postag,raw_entitys):
-		new_entitys=self.__pos_nh_to_ner_nh(postag,raw_entitys)
+		new_entitys = self.__pos_nh_to_ner_nh(postag,raw_entitys)
 		return new_entitys
 
 
@@ -74,32 +64,61 @@ class NamedEntityReg:
 	将postag词性为nh的词判定为实体Nh
 	"""
 	def __pos_nh_to_ner_nh(self,postag,raw_entitys):
-		new_entitys=raw_entitys
-		pos_nh_lists=self.__find_pos_nh_lists(postag)
+		new_entitys = list(raw_entitys)
+		pos_nh_lists = self.__find_pos_nh_lists(postag)
 		for t in pos_nh_lists:
-			for i in range(len(t)-1):
-				new_entitys[t[i]]='I-Nh'
-			if len(t)==2:
-				new_entitys[t[0]]='S-Nh'
+			for i in range(len(t) - 1):
+				new_entitys[t[i]] = "I-Nh"
+			if len(t) == 2:
+				new_entitys[t[0]] = "S-Nh"
 			else:
-				 new_entitys[t[0]]='B-Nh'
-				 new_entitys[t[len(t)-2]]='E-Nh'
+				 new_entitys[t[0]] = "B-Nh"
+				 new_entitys[t[len(t)-2]] = "E-Nh"
 		return new_entitys
 		
 
 	def __find_pos_nh_lists(self,postag):
-		L=[]
-		index=0
-		while index <len(postag):
-			t=[]
-			if postag[index]=='nh':
-				while index < len(postag) and postag[index]=='nh':
+		L = []
+		index = 0
+		while index < len(postag):
+			t = []
+			if postag[index] == "nh":
+				while index < len(postag) and postag[index] == "nh":
 					t.append(index)
-					index+=1
+					index += 1
 				t.append(index)				
 				L.append(t)
-			index+=1
+			index += 1
 		return L
+
+	"""
+	对于《》中的词，做强制分词, 同时看做是nz词性(如果本来没有识别)
+	返回新的分词、新词性列表
+	"""
+	def __optimize_segment(self,words,postag):
+		new_words = list(words)
+		new_postag = list(postag)
+		index = 0
+		first = -1
+		while index < len(new_words):
+			if new_words[index] == "《":
+				first = index
+			elif new_words[index] == "》" and first != -1 and index - first > 2:
+				last = index
+				work_name = ""
+				index = first + 1
+				while index < last:
+					work_name += new_words[index]
+					new_words.pop(index)
+					new_postag.pop(index)
+					last -= 1
+				new_words.insert(first + 1,work_name)
+				new_postag.insert(first + 1,"nz")
+				index = first + 2
+				first = -1
+			index += 1
+		return new_words,new_postag
+
 
 
 
@@ -130,7 +149,6 @@ if __name__ == "__main__":
 		raw_entitys , entitys = ner.recognize(text,"","")
 		print(raw_entitys,"\n")
 		print(entitys,"\n")
-		print(ner.parse(words,postag))
 		print("\n")
 		print("\n")
 
