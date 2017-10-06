0f1519123a4dd92d562f487bc53773369f2ea9ec
Haoran Li <lhrshitc@163.com>
2017-10-06 19:32:07 +0800

    v1

--- src/mapping/classify/merge_classify_result.py
+++ src/mapping/classify/merge_classify_result.py
@@ -44,6 +44,7 @@ if __name__ == "__main__":
         fb_types = fb_type_map.get(fb_uri, [])
 
         type_probs = type_infer.infer(bk_info, bk_clses)
+        # type_infer.choose_one_music_type(type_probs, 0.8)
         top_types = topk_key(type_probs, 2)
         
         match = False
@@ -57,4 +58,5 @@ if __name__ == "__main__":
             print bk_url, fb_uri, top_types, fb_types
     print "#extra mappings = %d" %extra_mappings
     outf.close()
+
 
