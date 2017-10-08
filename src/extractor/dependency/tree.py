7c8b8e0a408c493333c379a5a11faad063d26c54
Wang Yanbin <wangyanbin@WangdeMacBook-Pro.local>
2017-10-08 16:52:30 +0800

    update

--- src/extractor/dependency/tree.py
+++ src/extractor/dependency/tree.py
@@ -48,7 +48,6 @@ class ParseTree:
         while node.idx != father.idx:
             node = node.father
             path.append(node)
-        return path
     def find_path_to_root(self, node):
         path = [node]
         while node.father != None:
@@ -113,10 +112,32 @@ class ParseTree:
             for i in final_res:
                 res.append(process_father[0].index(i))
             return process_father[0][min(res)]
+    def nearest_verb(self, path):
+        res = None
+        for node in path:
+            if node.postag == 'v':
+                res = node
+                break
+        if res == None:
+            print "can't find nearest verb!"
+        return res
+    def judge_coo(self, verb1, verb2):
+        if verb1.father == verb2 and verb1.rel == 'COO':
+            return True
+        if verb2.father == verb1 and verb2.rel == 'COO':
+            return True
+        return False
+    def judge_one_SBV(self, near_verb1, near_verb2):
+        if near_verb1.rel == 'SBV' and near_verb2.rel != 'SBV':
+            return near_verb2.father
+        elif near_verb2.rel == 'SBV' and near_verb1.rel != 'SBV': 
+            return near_verb1.father
+        return None
     def find_path_verbs(self, word1, word2):
         w1 = self.find_coo_father(self.find_index(word1))
         w2 = self.find_coo_father(self.find_index(word2))
         simple_res = []
+        res = []
         if w1 == -1:
             print word1, "not recognized!"
         if w2 == -1:
@@ -124,10 +145,37 @@ class ParseTree:
         else:
             p1, p2 = self.find_path(w1, w2)
             coo_p = p1 + p2[:-1]
-            for node in coo_p:
-                if node.postag == 'v':
-                    simple_res.append(node)
-        return simple_res
+            for i in coo_p:
+                if i.postag == 'v':
+                    simple_res.append(i)
+            p1 = p1[1:]
+            p2 = p2[1:]
+            verb1 = self.nearest_verb(p1)
+            verb2 = self.nearest_verb(p2)
+            if verb1 == None or verb2 == None:
+                return simple_res, []
+            rel1 = verb1.rel
+            rel2 = verb2.rel
+            for i in p2:
+                print i.word
+            near_verb1 = self.find_path_to_father(self.nodes[w1], verb1)[-2]
+            near_verb2 = self.find_path_to_father(self.nodes[w2], verb2)[-2]
+            if rel1 == rel2:
+                print "the relation to verb is same! not found!"
+            elif verb1 == verb2:
+                print "same verb found!"
+                res = verb1
+            else:
+                if self.judge_coo(verb1, verb2):
+                    one_SBV = self.judge_one_SBV(near_verb1, near_verb2)
+                    if one_SBV != None:
+                        print "coo verbs and one SBV found!"
+                        res = one_SBV
+                    else:
+                        print "coo verbs but not one SBV! not found!"
+                else:
+                    print "other situation not found!"
+        return simple_res, res
     
 class Node:
     def __init__(self, idx, postag, arc, word = None, nertag = None):
@@ -147,10 +195,9 @@ class Node:
         else:
             self.depth = self.father.search_depth() + 1
         return self.depth
-
-def SimpleExtractor(sentence,ner1,ner2):
-    ltp = LTP(None)
+def Extractor(sentence, ner1, ner2):
     print sentence
+    ltp = LTP(None)
     ltp_result = ltp.parse(sentence)
     print '#' * 20
     print "words num:", len(ltp_result.words)
@@ -164,14 +211,18 @@ def SimpleExtractor(sentence,ner1,ner2):
         else:
             print ltp_result.words[arc.head], "--", arc.relation, "--", ltp_result.words[i]
     tree = ParseTree(ltp_result)
-    simple_res = tree.find_path_verbs(ner1, ner2)
-    print '*'*25
-    print "simple res :"
+    simple_res, advanced_res = tree.find_path_verbs(ner1, ner2)
+    print '*' * 25
+    print "simple res:"
     for i in range(len(simple_res)):
-        print "verb",i,":", simple_res[i].word, ", postag: ", simple_res[i].postag
+        print "verb",i,":", simple_res[i].word
+    print '*' * 25
+    print "advanced res:"
+    for i in range(len(advanced_res)):
+        print "verb",i,":", advanced_res[i].word
     return simple_res
+
 if __name__ == "__main__":
-    SimpleExtractor('截至2016年8月，巴萨在西班牙国内，共赢得了24次西甲联赛冠军、\
-    28次国王杯（在国王杯历史上高居榜首）、12座西班牙超级杯、2座伊娃杯和2座西班牙联赛杯', '巴萨', '西班牙超级杯') 
-    
-    
+    Extractor('截至2016年8月，巴萨在西班牙国内，共赢得了24次西甲联赛冠军、\
+    28次国王杯（在国王杯历史上高居榜首）、12座西班牙超级杯、2座伊娃杯和2座西班牙联赛杯',\
+    '巴萨', '西班牙联赛杯')
