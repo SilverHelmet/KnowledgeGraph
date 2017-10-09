e4df484c18f9cbd66633b1d20ee6551fb6d2ba2b
Wang Yanbin <wangyanbin@WangdeMacBook-Pro.local>
2017-10-09 12:25:44 +0800

    update

--- src/extractor/dependency/tree.py
+++ src/extractor/dependency/tree.py
@@ -80,96 +80,45 @@ class RelationExtractor:
             node = node.father
             path.append(node)
         return path
-    
-    def find_index(self, tree, word):
-        unigram = []
-        bigram = []
-        trigram = []
-        quagram = []
-        fifgram = []
-        sixgram = []
-        lens = tree.length
-        for i in range(lens):
-            unigram.append(tree.nodes[i].word)
-        for i in range(lens - 1):
-            bigram.append(''.join(unigram[i : i + 2]))
-        for i in range(lens - 2):
-            trigram.append(''.join(unigram[i : i + 3]))
-        for i in range(lens - 3):
-            quagram.append(''.join(unigram[i : i + 4]))
-        for i in range(lens - 4):
-            fifgram.append(''.join(unigram[i : i + 5]))
-        for i in range(lens - 5):
-            sixgram.append(''.join(unigram[i : i + 6]))
-        if word in unigram:
-            return [1, unigram.index(word)]
-        elif word in bigram:
-            return [2, bigram.index(word)]
-        elif word in trigram:
-            return [3, trigram.index(word)]
-        elif word in quagram:
-            return [4, quagram.index(word)]
-        elif word in fifgram:
-            return [5, fifgram.index(word)]
-        elif word in sixgram:
-            return [6, sixgram.index(word)]
-        else:
-            return [0, None]
         
-    def find_coo_father(self, tree, lis):
-        if lis[0] == 0:
-            return -1
-        else:
-            lens = lis[0]
-            st_index = lis[1]
-            words = []
-            for i in range(lens):
-                words.append(tree.nodes[st_index + i])
-            coo_father = []
-            final_res = []
-            res = []
-            for i in range(lens):
-                coo_father.append(self.find_path_to_root(words[i]))
-            process_father = []
-            for i in range(lens):
-                process_father.append([])
-                for j in range(len(coo_father[i])):
-                    process_father[i].append(coo_father[i][j].idx)
-            index_father = []
-            for i in range(lens):
-                index_father += process_father[i]
-            father = set(index_father)
-            for num in father:
-                if(index_father.count(num) == lens):
-                    final_res.append(num)
-            for i in final_res:
-                res.append(process_father[0].index(i))
-            return process_father[0][min(res)]
-        
+    def find_coo_father(self, nodes):
+        coo_father = []
+        process_father = []
+        final_res = []
+        res = []
+        for i in range(len(nodes)):
+            coo_father.append(self.find_path_to_root(nodes[i]))
+        for i in range(len(nodes)):
+            process_father.append([])
+            for j in range(len(coo_father[i])):
+                process_father[i].append(coo_father[i][j].idx)
+        index_father = []
+        for i in range(len(nodes)):
+            index_father += process_father[i]
+        father = set(index_father)
+        for num in father:
+            if(index_father.count(num) == len(nodes)):
+                final_res.append(num)
+        for i in final_res:
+            res.append(process_father[0].index(i))
+        return process_father[0][min(res)]
+
     def find_path_verbs(self, ltp_result, e1, e2, entity_pool):
         tree = ParseTree(ltp_result)
-        word1 = word2 = ''
-        for i in range(e1.st, e1.ed):
-            word1 += tree.nodes[i].word
-        for i in range(e2.st, e2.ed):
-            word2 += tree.nodes[i].word
-        w1 = self.find_coo_father(tree, self.find_index(tree, word1))
-        w2 = self.find_coo_father(tree, self.find_index(tree, word2))
+        node_list_1 = tree.nodes[e1.st : e1.ed]
+        node_list_2 = tree.nodes[e2.st : e2.ed]
+        w1 = self.find_coo_father(node_list_1)
+        w2 = self.find_coo_father(node_list_2)
         simple_res = []
-        if w1 == -1:
-            print word1, "not recognized!"
-        if w2 == -1:
-            print word2, "not recognized!"
-        else:
-            p1 = self.find_path_to_root(tree.nodes[w1])
-            p2 = self.find_path_to_root(tree.nodes[w2])
-            coo_p = p1 + p2[:-1]
-            for node in coo_p:
-                if node.postag == 'v':
-                    if entity_pool[node.idx] == 1:
-                        print node.word,": extract verb is an entity! error!"
-                    else:
-                        simple_res.append([(node.idx, node.idx + 1)])
+        p1 = self.find_path_to_root(tree.nodes[w1])
+        p2 = self.find_path_to_root(tree.nodes[w2])
+        coo_p = p1 + p2[:-1]
+        for node in coo_p:
+            if node.postag == 'v':
+                if entity_pool[node.idx] == 1:
+                    print node.word,": extract verb is an entity! error!"
+                else:
+                    simple_res.append([(node.idx, node.idx + 1)])
         return simple_res
     
 class prenode:
@@ -189,10 +138,3 @@ if __name__ == "__main__":
     res = RelationExtractor()
     tmp = res.find_path_verbs(ltp_result, e1, e2, entity_pool)
     print tmp
-    '''
-    simple_res = SimpleExtract(ltp_result, e1, e2, entity_pool)
-    print '*' * 25
-    print "simple res :"
-    for i in range(len(simple_res)):
-        print "verb", i, ":", tree.nodes[simple_res[i][0]].word
-    '''
