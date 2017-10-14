ea07e9547ca25486a6db7adb2619e84166e32887
stupidlulu <dywzc123@163.com>
2017-10-14 21:42:45 +0800

    debug

--- src/mapping/classify/merge_classify_result.py
+++ src/mapping/classify/merge_classify_result.py
@@ -31,10 +31,12 @@ if __name__ == "__main__":
     fb_type_map = load_fb_type(filepath = os.path.join(classify_dir, 'fb_entity_type.json'), fb_uris = fb_uris) 
     bk_info_map = load_baike_attr_names(filepath = os.path.join(result_dir, '360/360_entity_info_processed.json'),
                                          total = 21710208, baike_urls = bk_urls)
+    bk_title_map = load_baike_entity_title()
 
-    infobox_path = os.path.join(result_dir, '360/mapping/one2one_predicates_map.json')
+    baike_title_path = os.path.join(result_dir, '360/title_type.txt')
+    baike_infobox_path = os.path.join(result_dir, '360/info_type.txt')
     baike_cls_path = os.path.join(classify_dir, 'baike_cls2fb_type.json')
-    type_infer = TypeInfer(infobox_path = infobox_path, baike_cls_path = baike_cls_path)
+    type_infer = TypeInfer(baike_info_path = baike_infobox_path, baike_cls_path = baike_cls_path, baike_title_path = baike_title_path)
 
     extra_mappings = 0
 
@@ -42,8 +44,9 @@ if __name__ == "__main__":
         bk_clses = bk_cls_map.get(bk_url, [])
         bk_info = bk_info_map.get(bk_url, [])
         fb_types = fb_type_map.get(fb_uri, [])
+        bk_title = bk_title_map.get(fb_uri, [])
 
-        type_probs = type_infer.infer(bk_info, bk_clses)
+        type_probs = type_infer.infer(bk_info, bk_clses, bk_title)
         # type_infer.choose_one_music_type(type_probs, 0.8)
         top_types = topk_key(type_probs, 2)
         
