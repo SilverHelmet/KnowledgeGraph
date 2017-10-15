a78ab7f4bd935b8b2f56e2f503aeeeae120a928b
stupidlulu <dywzc123@163.com>
2017-10-15 09:55:04 +0800

    debug

--- scripts/classify.sh
+++ scripts/classify.sh
@@ -6,7 +6,7 @@ python -u -m src.mapping.classify.calc_infobox_mapping_score >& log/calc_mapping
 python -u -m src.mapping.classify.gen_good_one2one_mapping >& log/gen_good_one2one.log
 python -u -m src.mapping.classify.gen_baike_class_to_fb
 #python -u -m src.mapping.predicate_mapping >& log/predicate_mapping.log
-python -m src.mapping.collect_predicate_mapping_result
+#python -m src.mapping.collect_predicate_mapping_result
 
 # classify
 python -u -m src.mapping.classify.simple_classify >& log/simple_classify.log
