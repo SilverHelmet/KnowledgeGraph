1bf260d20d6bd68da6fade54d57b9c1287ffeca6
Haoran Li <lhrshitc@163.com>
2017-10-19 03:19:29 +0800

    update

--- scripts/extract_relation.sh
+++ scripts/extract_relation.sh
@@ -1,5 +1,10 @@
-python -m src.baike_process.process_page $1 result/cache/sentences.txt result/cache/documents.json
+sentence_path="result/cache/sentences.txt"
+document_path="result/cache/documents.json"
+stf_result_path="result/cache/stanford_results.txt"
+python -m src.baike_process.process_page $1 %sentence_path $document_path
 
 cd java
-java -mx3g -cp "*:lib/*:." ChNlp_v2 ../result/cache/sentences.txt ../result/cache/stanford_results.txt
+java -mx3g -cp "*:lib/*:." ChNlp_v2 "../${sentence_path}" "../${stf_result_path}"
 cd ..
+
+python -m src.extractor.main "${document_path}" "${sentence_path}" "${stf_result_path}" $2
