6dfa28cf77269af06852cdd99e1a93c670e9296f
Haoran Li <lhrshitc@163.com>
2017-11-20 00:38:12 +0800

    gen dict scripts

--- scripts/gen_dict.sh
+++ scripts/gen_dict.sh
@@ -1,2 +1,3 @@
-python -u -m src.rel_extraction.extract_baike_names >& log/extract_baike_names.log
-python -u -m src.rel_extraction.gen_dict >& log/gen_dict.log
+python -u -m src.rel_extraction.dict.gen_title_rel_dict >& log/gen_title_rel_dict.log 
+python -u -m src.rel_extraction.dict.extra_profession_dict >& log/extra_prof.log
+python -u -m src.rel_extraction.dict.gen_location_dict >& log/gen_location_dict.log 
