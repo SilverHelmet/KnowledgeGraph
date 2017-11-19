2983bcfed3b991d4c3815a7268c8f9a58c8b6712
Haoran Li <lhrshitc@163.com>
2017-11-19 17:39:18 +0800

    add restriction

--- src/rel_extraction/dict/gen_title_rel_dict.py
+++ src/rel_extraction/dict/gen_title_rel_dict.py
@@ -1,5 +1,5 @@
 #encoding: utf-8
-from ..IOUtil import rel_ext_dir, Print, result_dir, doc_dir
+from src.IOUtil import rel_ext_dir, Print, result_dir, doc_dir
 from tqdm import tqdm
 import os 
 from src.util import is_chinese
