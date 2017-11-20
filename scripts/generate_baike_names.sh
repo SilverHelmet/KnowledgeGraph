02c5794e01c6b7ab7a2862f425dfb73640fc8773
Haoran Li <lhrshitc@163.com>
2017-11-20 22:58:17 +0800

    add domains

--- scripts/generate_baike_names.sh
+++ scripts/generate_baike_names.sh
@@ -1,5 +1,7 @@
 # python -u -m src.rel_extraction.extract_baike_names >& log/extract_baike_names.log
 python -u -m src.rel_extraction.dict.gen_vertical_domain_dict >& log/gen_vertical_dict.log
-python -u -m src.rel_extraction.dict.gen_title_rel_dict >& log/gen_title_rel_dict.log 
-python -u -m src.rel_extraction.dict.extra_profession_dict >& log/extra_prof.log
-python -u -m src.rel_extraction.dict.gen_location_dict >& log/gen_location_dict.log 
+# python -u -m src.rel_extraction.dict.gen_title_rel_dict >& log/gen_title_rel_dict.log 
+# python -u -m src.rel_extraction.dict.extra_profession_dict >& log/extra_prof.log
+# python -u -m src.rel_extraction.dict.gen_location_dict >& log/gen_location_dict.log 
+
+python -u -m src.extractor.test.test_entity_linking >& log/test_entity_linking.log11
