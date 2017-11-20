95fa28667448a82af775765fa811510d4fbfb01b
Haoran Li <lhrshitc@163.com>
2017-11-20 17:03:58 +0800

    fix bug

--- scripts/generate_baike_names.sh
+++ scripts/generate_baike_names.sh
@@ -1,4 +1,4 @@
-python -u -m src.rel_extraction.extract_baike_names >& log/extract_baike_names.log
+# python -u -m src.rel_extraction.extract_baike_names >& log/extract_baike_names.log
 python -u -m src.rel_extraction.dict.gen_vertical_domain_dict >& log/gen_vertical_dict.log
 python -u -m src.rel_extraction.dict.gen_title_rel_dict >& log/gen_title_rel_dict.log 
 python -u -m src.rel_extraction.dict.extra_profession_dict >& log/extra_prof.log
