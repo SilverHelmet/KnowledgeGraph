20b783def135f98bc433a98f25aa0a592f829324
Haoran Li <lhrshitc@163.com>
2017-11-23 13:33:15 +0800

    fix bug

--- src/rel_extraction/extract_summary_extra_name.py
+++ src/rel_extraction/extract_summary_extra_name.py
@@ -335,7 +335,7 @@ def unfold(extra_name, ori_names):
     return [extra_name]
 
 
-strange_puncs = [u' ', u'\t', u'\n', u'"', u'\'', u':', u'：', u')', u'(', u'）', u'）', u'”', u'“']
+strange_puncs = [u' ', u'\t', u'\n', u'"', u'\'', u':', u'：', u')', u'(', u'）', u'）', u'”', u'“', u']',u'[',u'】',u'【']
 bracket_puncs = [u'《', u'》']
 def has_strange_punc(extra_name):
     global strange_puncs, brackets
