4d43cb8f05dc747e3335333cd8e6348942501243
Haoran Li <lhrshitc@163.com>
2017-11-27 14:41:00 +0800

    add docs

--- scripts/grep_doc.sh
+++ scripts/grep_doc.sh
@@ -1,15 +1,9 @@
 urls=" \
-baike.so.com/doc/6707957-6921972.html \
-baike.so.com/doc/5382393-5618748.html \
-baike.so.com/doc/5348516-5583969.html \
 baike.so.com/doc/1287918-1361771.html \
+baike.so.com/doc/2526484-2669235.html \
+baike.so.com/doc/5382393-5618748.html \
 baike.so.com/doc/4835393-5052275.html \
-baike.so.com/doc/5385244-5621690.html \
-baike.so.com/doc/1957541-2071736.html \
-baike.so.com/doc/5344366-5579811.html \
-baike.so.com/doc/574984-608736.html \
-baike.so.com/doc/5329601-5564775.html \
-baike.so.com/doc/3433066-3613205.html
+baike.so.com/doc/6662392-6876216.html
 "
 
 sample_dir="result/samples"
