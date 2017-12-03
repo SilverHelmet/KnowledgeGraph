2198dc92d28953ae6f3948e044eeb991c3dba490
Haoran Li <lhrshitc@163.com>
2017-12-03 22:24:18 +0800

    update

--- scripts/extract_relation.sh
+++ scripts/extract_relation.sh
@@ -1,10 +1,3 @@
-sentence_path="result/cache/sentences.txt"
 document_path="result/cache/documents.json"
-stf_result_path="result/cache/stanford_results.txt"
 python -m src.baike_process.process_page $1 "$document_path"
-
-cd java
-java -mx3g -cp "*:lib/*:." ChNlp_v2 "../${sentence_path}" "../${stf_result_path}"
-cd ..
-
-python -m src.extractor.main "${document_path}" "${sentence_path}" "${stf_result_path}" $2
+python -m src.extractor.main "${document_path}" $2
